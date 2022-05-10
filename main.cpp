b3b28474fa2de3e36e31b3ab020fd20295144abb
Astronomax <fgfgfb93@gmail.com>
2022-05-10 17:59:24 +0300

    Add some optimizations

--- main.cpp
+++ main.cpp
@@ -1,253 +1,301 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 #include <set>
-#include <cassert>
 #include <map>
 #include <queue>
+#include <algorithm>
 
 using namespace std;
 
-int clauses_cnt, vars_cnt;
-const int CLAUSES_LIMIT = 10000;
-const int VARS_LIMIT = 1000;
-vector<int> clauses[CLAUSES_LIMIT];
-int sign[CLAUSES_LIMIT][VARS_LIMIT];
+typedef pair<int, int> literal;
+typedef vector<literal> clause;
 
-int values[VARS_LIMIT];
-int decision_level = 0;
-int decision_literals[VARS_LIMIT];
-int trails[VARS_LIMIT];
-
-int assigned_vars_cnt = 0;
-int assignation_time[VARS_LIMIT];
-int assignation_order[VARS_LIMIT];
-vector<int> implication_graph[VARS_LIMIT];
-
-int check_conflicts() {
-    for (int i = 0; i < clauses_cnt; i++) {
-        bool unsatisfied = true;
-        for (int j : clauses[i]) {
-            if (values[j] == -1)
-                unsatisfied = false;
-            if (sign[i][j] == 2)
-                unsatisfied = false;
-            if (sign[i][j] == values[j])
-                unsatisfied = false;
-        }
-        if (unsatisfied) return i;
+class solver {
+public:
+    void fit(const vector<clause> &c) {
+        solved = false;
+        clear();
+        for (auto &i : c)
+            add_clause(i);
+        // TODO compress coords
     }
-    return -1;
-}
 
-int unit_propagate() {
-    for (int i = 0; i < clauses_cnt; i++) {
-        set<int> unassigned_vars;
-        for (int j : clauses[i])
-            if (values[j] == -1)
-                unassigned_vars.insert(j);
-        bool other_is_false = true;
-        for (int j : clauses[i]) {
-            if (values[j] == -1)
-                continue;
-            if (sign[i][j] == 2)
-                other_is_false = false;
-            if (values[j] == sign[i][j])
-                other_is_false = false;
+    bool solve() {
+        if (solved) return ans;
+        solved = true;
+
+        decision_level = 0;
+        level_time[decision_level] = -1;
+        while (!unassigned.empty()) {
+            int conflict = unit_propagate();
+            if (conflict != -1) {
+                if (decision_level == 0)
+                    return ans = false;
+                clause c = analyze_conflict(conflict);
+                back_jump(c);
+                add_clause(c);
+            }
+            else if (!unassigned.empty())
+                make_new_decision();
+        }
+        return ans = true;
+    }
+
+private:
+    int unit_propagate() {
+        while (!units.empty()) {
+            int c = *units.begin();
+            literal l = *clause_unassigned_literals[c].begin();
+            set_value(l);
+            assignation_order.push_back(l.first);
+            level[l.first] = decision_level;
+            for (auto &i : clauses[c]) {
+                if (i == l) continue;
+                implications[i.first].insert(l.first);
+                implications_t[l.first].insert(i.first);
+            }
+            if (!conflicts.empty())
+                return *conflicts.begin();
+        }
+        return -1;
+    }
+
+    clause analyze_conflict(int conflict) {
+        clause res;
+        for (auto &i : clauses[conflict])
+            implications[i.first].insert(-1); //edges to bottom
+
+        map<int, bool> is_pred;
+        is_pred[-1] = true;
+        for (int i = (int)assignation_order.size() - 1; i >= max(0, level_time[decision_level]); i--) {
+            int current_var = assignation_order[i];
+            for (auto &j : implications[current_var])
+                is_pred[current_var] |= is_pred[j];
         }
 
-        if (other_is_false && unassigned_vars.size() == 1) {
-            int var = *unassigned_vars.begin();
-            if (sign[i][var] != 2) {
-                set<int> preds;
-                for (int j : clauses[i])
-                    if (values[j] != -1)
-                        preds.insert(j);
-                for (auto &j : preds)
-                    implication_graph[j].push_back(var);
-                trails[var] = decision_level;
-                values[var] = sign[i][var];
-                assignation_order[assigned_vars_cnt] = var;
-                assignation_time[var] = assigned_vars_cnt;
-                ++assigned_vars_cnt;
-                return var;
+        int last_decision = assignation_order[level_time[decision_level]];
+        int UIP = last_decision;
+        for (int i = level_time[decision_level]; i < (int)assignation_order.size(); i++) {
+            int current_var = assignation_order[i];
+            if (is_pred[current_var]) {
+                bool ok = true;
+                queue<int> que;
+                set<int> used;
+                que.push(last_decision);
+                while (!que.empty()) {
+                    int v = que.front();
+                    que.pop();
+                    if (used.find(v) != used.end())
+                        continue;
+                    used.insert(v);
+                    if (v == current_var) continue;
+                    else if (v == -1) ok = false;
+                    for (auto &j : implications[v])
+                        que.push(j);
+                }
+                if (ok) UIP = current_var;
             }
         }
-    }
-    return -1;
-}
 
-bool is_UIP(int from, int u) {
-    queue<int> que;
-    que.push(from);
-    while (!que.empty()) {
-        int v = que.front();
-        que.pop();
-        if (v == u) continue;
-        else if (v == -1) return false;
-        for (auto &j : implication_graph[v])
+        set<int> B;
+        queue<int> que;
+        set<int> used;
+        for (auto &j : implications[UIP])
             que.push(j);
-    }
-    return true;
-}
+        while (!que.empty()) {
+            int v = que.front();
+            que.pop();
+            if (used.find(v) != used.end())
+                continue;
+            used.insert(v);
+            if (v != -1)
+                for (auto &j : implications[v])
+                    que.push(j);
+            B.insert(v);
+        }
 
-vector<pair<int, int>> analyze_conflict(int falsified) {
-    for (auto &j : clauses[falsified])
-        implication_graph[j].push_back(-1);
-
-    vector<pair<int, int>> clause;
-    int last_decision = decision_literals[decision_level];
-    int last_decision_time = assignation_time[last_decision];
-
-    map<int, bool> is_pred;
-    is_pred[assignation_order[assigned_vars_cnt - 1]] = true;
-    for (int i = assigned_vars_cnt - 2; i >= last_decision_time; i--) {
-        int current_var = assignation_order[i];
-        for (auto &j : implication_graph[current_var])
-            is_pred[current_var] |= is_pred[j];
-    }
-
-    int UIP = last_decision;
-    for (int i = last_decision_time; i < assigned_vars_cnt; i++) {
-        int current_var = assignation_order[i];
-        if (is_UIP(last_decision, current_var))
-            UIP = current_var;
-    }
-
-    set<int> B;
-    queue<int> que;
-    for (auto &j : implication_graph[UIP])
-        que.push(j);
-    while (!que.empty()) {
-        int v = que.front();
-        que.pop();
-        if(v != -1)
-            for (auto &j : implication_graph[v])
-                que.push(j);
-        B.insert(v);
-    }
-
-    for (int i = 0; i < assigned_vars_cnt; i++) {
-        int v = assignation_order[i];
-        if (trails[v] == 0) continue;
-        if (B.find(v) == B.end()) {
-            for (auto &j : implication_graph[v]) {
-                if (B.find(j) != B.end()) {
-                    clause.emplace_back(v, 1 - values[v]);
-                    break;
+        for (int v : assignation_order) {
+            if (B.find(v) == B.end()) {
+                for (auto &j : implications[v]) {
+                    if (B.find(j) != B.end()) {
+                        res.emplace_back(v, 1 - values[v]);
+                        break;
+                    }
                 }
             }
         }
+        for (auto &i : clauses[conflict])
+            implications[i.first].erase(-1); //edges to bottom
+        return res;
     }
-    for (auto &j : clauses[falsified])
-        implication_graph[j].pop_back();
-    return clause;
-}
 
-void back_jump(const vector<pair<int, int>> &clause) {
-    int level = 0;
-    for (auto &j : clause)
-        if (trails[j.first] < decision_level)
-            level = max(level, trails[j.first]);
-
-    int to = (level == 0) ? -1 : assignation_time[decision_literals[level]];
-
-    while (assigned_vars_cnt - 1 > to) {
-        implication_graph[assignation_order[assigned_vars_cnt - 1]].clear();
-        assignation_time[assignation_order[assigned_vars_cnt - 1]] = -1;
-        values[assignation_order[assigned_vars_cnt - 1]] = -1;
-        trails[assignation_order[assigned_vars_cnt - 1]] = -1;
-        assignation_order[assigned_vars_cnt - 1] = -1;
-        --assigned_vars_cnt;
+    void back_jump(const clause &c) {
+        pair<int, int> max_levels = make_pair(0, 0);
+        for (auto &j : c) {
+            if (level[j.first] < decision_level) {
+                if (level[j.first] > max_levels.second) {
+                    max_levels.first = max_levels.second;
+                    max_levels.second = level[j.first];
+                }
+                else if (level[j.first] > max_levels.first)
+                    max_levels.first = level[j.first];
+            }
+        }
+        while ((int)assignation_order.size() - 1 > level_time[max_levels.first]) {
+            int current_var = *assignation_order.rbegin();
+            level[current_var] = -1;
+            for (auto &i : implications_t[current_var])
+                implications[i].erase(current_var);
+            implications_t[current_var].clear();
+            reset_value(current_var);
+            assignation_order.pop_back();
+        }
+        decision_level = max_levels.first;
     }
-    for (int i = decision_level; i > level; i--)
-        decision_literals[i] = -1;
-    decision_level = level;
-}
 
-void make_new_decision() {
-    int var = 0;
-    for (int i = 0; i < vars_cnt; i++)
-        if (values[i] == -1)
-            var = i;
-    values[var] = 0;
-    assignation_order[assigned_vars_cnt] = var;
-    assignation_time[var] = assigned_vars_cnt;
-    ++assigned_vars_cnt;
-    decision_literals[++decision_level] = var;
-}
+    void make_new_decision() {
+        int var = *unassigned.begin();
+        set_value(make_pair(var, 0));
+        assignation_order.push_back(var);
+        ++decision_level;
+        level[var] = decision_level;
+        level_time[decision_level] = (int)assignation_order.size() - 1;
+    }
 
-void add_clause(const vector<pair<int, int>>& clause) {
-    vector<pair<bool, bool>> presented(VARS_LIMIT, make_pair(false, false));
-    for (auto &i : clause) {
-        if (i.second == 0)
-            presented[i.first].first = true;
-        else presented[i.first].second = true;
+    static literal get_opposite(literal l) {
+        return make_pair(l.first, 1 - l.second);
     }
-    for (int i = 0; i < VARS_LIMIT; i++) {
-        if (presented[i].first && presented[i].second)
-            sign[clauses_cnt][i] = 2;
-        else if (presented[i].first)
-            sign[clauses_cnt][i] = 0;
-        else if (presented[i].second)
-            sign[clauses_cnt][i] = 1;
+
+    void reset_value(int var) {
+        unassigned.insert(var);
+        literal l = make_pair(var, values[var]);
+        values[var] = -1;
+        for (auto &i : literal_clauses[l]) {
+            --true_literals[i];
+            clause_unassigned_literals[i].insert(l);
+            if (clause_unassigned_literals[i].size() == 1)
+                if (!true_literals[i])
+                    units.insert(i);
+        }
+        for (auto &i : literal_clauses[get_opposite(l)]) {
+            if (clause_unassigned_literals[i].empty()) {
+                if (!true_literals[i]) {
+                    units.insert(i);
+                    conflicts.erase(i);
+                }
+            }
+            else if (clause_unassigned_literals[i].size() == 1) {
+                if (!true_literals[i])
+                    units.erase(i);
+            }
+            --false_literals[i];
+            clause_unassigned_literals[i].insert(get_opposite(l));
+        }
     }
-    assert(clauses_cnt < CLAUSES_LIMIT);
-    clauses[clauses_cnt].resize(clause.size());
-    for (int i = 0; i < clause.size(); i++)
-        clauses[clauses_cnt][i] = clause[i].first;
-    ++clauses_cnt;
-}
+
+    void set_value(literal l) {
+        unassigned.erase(l.first);
+        values[l.first] = l.second;
+        for (auto &i : literal_clauses[l]) {
+            if (clause_unassigned_literals[i].size() == 1)
+                if (!true_literals[i])
+                    units.erase(i);
+            ++true_literals[i];
+            clause_unassigned_literals[i].erase(l);
+        }
+        for (auto &i : literal_clauses[get_opposite(l)]) {
+            ++false_literals[i];
+            clause_unassigned_literals[i].erase(get_opposite(l));
+            if (clause_unassigned_literals[i].empty()) {
+                if (!true_literals[i]) {
+                    units.erase(i);
+                    conflicts.insert(i);
+                }
+            }
+            else if (clause_unassigned_literals[i].size() == 1) {
+                if (!true_literals[i])
+                    units.insert(i);
+            }
+        }
+    }
+
+    void add_clause(clause c) {
+        sort(c.begin(), c.end());
+        c.erase(unique(c.begin(), c.end()), c.end());
+        for (auto &i : c) {
+            literal_clauses[i].push_back((int)clauses.size());
+            if (!values.count(i.first) || values[i.first] == -1) {
+                unassigned.insert(i.first);
+                clause_unassigned_literals[(int)clauses.size()].insert(i);
+            }
+            else {
+                if (values[i.first] == i.second)
+                    ++true_literals[(int)clauses.size()];
+                else ++false_literals[(int)clauses.size()];
+            }
+        }
+        if (clause_unassigned_literals[(int)clauses.size()].size() == 1)
+            units.insert((int)clauses.size());
+        clauses.push_back(c);
+    }
+
+    void clear() {
+        clauses.clear();
+        literal_clauses.clear();
+        true_literals.clear();
+        false_literals.clear();
+        clause_unassigned_literals.clear();
+        values.clear();
+        units.clear();
+        conflicts.clear();
+        unassigned.clear();
+        level.clear();
+        level_time.clear();
+        assignation_order.clear();
+        implications.clear();
+        implications_t.clear();
+    }
+
+private:
+    bool ans = false;
+    bool solved = false;
+
+    vector<clause> clauses;
+    map<literal, vector<int>> literal_clauses;
+    map<int, int> true_literals;
+    map<int, int> false_literals;
+    map<int, set<literal>> clause_unassigned_literals;
+    map<int, int> values;
+    set<int> units;
+    set<int> conflicts;
+    set<int> unassigned;
+
+    int decision_level = 0;
+    map<int, int> level;
+    map<int, int> level_time;
+    vector<int> assignation_order;
+    map<int, set<int>> implications;
+    map<int, set<int>> implications_t;
+};
 
 int main() {
-    //freopen("input.txt", "r", stdin);
-
-    fill(&sign[0][0], &sign[0][0] + CLAUSES_LIMIT * VARS_LIMIT, -1);
-    fill(&values[0], &values[0] + VARS_LIMIT, -1);
-    fill(&decision_literals[0], &decision_literals[0] + VARS_LIMIT, -1);
-    fill(&trails[0], &trails[0] + VARS_LIMIT, -1);
-    fill(&assignation_time[0], &assignation_time[0] + VARS_LIMIT, -1);
-    fill(&assignation_order[0], &assignation_order[0] + VARS_LIMIT, -1);
+    freopen("input.txt", "r", stdin);
 
     int n; cin >> n;
+    vector<clause> clauses;
     for (int i = 0; i < n; i++) {
         int k; cin >> k;
-        vector<pair<int, int>> clause(k);
+        clause c;
         for (int j = 0; j < k; j++) {
             int literal; cin >> literal;
             if (literal > 0)
-                clause[j] = make_pair(literal - 1, 1);
-            else clause[j] = make_pair(-literal - 1, 0);
+                c.push_back(make_pair(literal - 1, 1));
+            else c.push_back(make_pair(-literal - 1, 0));
         }
-        add_clause(clause);
-        for (auto &j : clause)
-            vars_cnt = max(vars_cnt, j.first + 1);
+        clauses.push_back(c);
     }
-    assert(vars_cnt < VARS_LIMIT);
-
-    while (assigned_vars_cnt < vars_cnt) {
-        bool no_conflicts = true;
-
-        while (unit_propagate() != -1) {
-            int falsified = check_conflicts();
-            if (falsified != -1) {
-                if (decision_level == 0) {
-                    cout << "UNSAT";
-                    return 0;
-                }
-                vector<pair<int, int>> c = analyze_conflict(falsified);
-                add_clause(c);
-                back_jump(c);
-                no_conflicts = false;
-                break;
-            }
-        }
-        if (no_conflicts && assigned_vars_cnt < vars_cnt) {
-            make_new_decision();
-            assert(check_conflicts() == -1);
-        }
-    }
-    cout << "SAT" << "\n";
-    for (int i = 0; i < vars_cnt; i++)
-        cout << values[i] << " ";
+    solver s;
+    s.fit(clauses);
+    cout << (s.solve() ? "SAT" : "UNSAT") << endl;
     return 0;
 }
